2abce53c1f2398e6a79443e448f5db7fc1ba7ef4
sidecode <sarkar.krishnan@gmail.com>
2016-10-08 17:07:49 -0400

    Add hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var Webpack = require('webpack');
 
 module.exports = {
   entry: [
     'babel-polyfill',
+    'webpack-dev-server/client?http://0.0.0.0:8080',
+    'webpack/hot/only-dev-server',
     './app/index.js'
   ],
   output: {
@@ -14,25 +17,21 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/, 
-        exclude: /node_modules/, 
+        exclude: "/node_modules/",
         loader: "babel-loader"
       },
       {
         test: /\.css$/,
+        exclude: "/node_modules/", 
         loader: ExtractTextPlugin.extract(
           'style-loader',
-          combineLoaders([{
-            loader: 'css-loader',
-            query: {
-              modules: true,
-              localIdentName: '[name]__[local]__[hash:base64:5]'
-            }
-          }])
+          'css-loader?modules&localIdentName=[name]__[local]__[hash:base64:5]'
         )
       }
     ]
   },
   plugins: [
+    new Webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: __dirname + '/app/index.html',
       filename: 'index.html',
