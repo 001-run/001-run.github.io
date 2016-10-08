8dc5f91d0cf4df56aab53011c0d15c861ac6e8c4
sidecode <sarkar.krishnan@gmail.com>
2016-10-08 13:22:03 -0400

    Add basic styling

--- app/components/PostList.js
+++ app/components/PostList.js
@@ -4,7 +4,7 @@ import styles from './style.css'
 export default class PostList extends Component {
   render() { return (
     <div className={styles.PostList}>
-      {this.props.posts}
+      {this.props.isLoading ? <p>LOADING</p> : this.props.posts}
     </div>
   )}
 }
